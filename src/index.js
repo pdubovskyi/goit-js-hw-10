8aece2fb0f32f058675b9f5b7419ccc58205fa4c
pdubovskyi <petro.dubovskyi@gmail.com>
2022-12-04 22:52:00 +0200

    task 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './css/styles.css';
+import Lodash from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryInfoEl = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', Lodash(onInputEl, DEBOUNCE_DELAY));
+
+function onInputEl(e) {
+  const name = e.target.value.trim();
+  console.log(name);
+
+  country(name);
+}
+
+function country(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(responce => {
+      return responce.json();
+    })
+    .then(renderCountry)
+    .catch(error => {
+      console.log('error!');
+    });
+}
+
+function renderCountry(countries) {
+  console.log(countries);
+  const markup = countries
+    .map(({ name, capital, population, flags, languages }) => {
+      return ` <img src="${flags.svg}" alt="${name.official}" width="50"> 
+      <h1>${name.official}</h1>
+        <p><b>Capital:</b> ${capital}</p>
+         <p><b>Population:</b> ${population}</p>
+         <p><b>Languages:</b> ${Object.values(languages)}</p>`;
+    })
+    .join('');
+  countryInfoEl.innerHTML = markup;
+}
