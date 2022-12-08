8e908f05ac20a2cd3a1b9902fe853380fc71afe4
pdubovskyi <petro.dubovskyi@gmail.com>
2022-12-08 23:06:49 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ function onInputEl(e) {
     .then(renderCountry)
     .catch(error => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
+      listOfCountries.innerHTML = '';
+      countryInfoEl.innerHTML = '';
     });
 }
 
